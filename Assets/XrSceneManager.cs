6b9d2954b33f1fbf5c834c44eaa2e80253ff16a7
Freddy Newton <freddakdogan2@gmail.com>
2025-01-03 21:34:10 +0100

    Fixed Ar Camera Passthrough

--- Assets/XrSceneManager.cs
+++ Assets/XrSceneManager.cs
@@ -1,22 +1,43 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
 public class XrSceneManager : MonoBehaviour
 {
     [SerializeField] private ARPlaneManager m_ArPlaneManager;
+    [SerializeField] private ARCameraManager m_ARCameraManager;
+    [SerializeField] private ARCameraBackground m_ARCameraBackground;
 
     private void Start()
     {
         m_ArPlaneManager ??= FindFirstObjectByType<ARPlaneManager>();
+        m_ARCameraManager ??= FindFirstObjectByType<ARCameraManager>();
+        m_ARCameraBackground ??= FindFirstObjectByType<ARCameraBackground>();
     }
 
     private void Awake()
     {
         SetArPlaneActive(true);
+        StartCoroutine(FadePassthrough(true));
     }
 
     public void SetArPlaneActive(bool active)
     {
         m_ArPlaneManager.enabled = active;
+
+        if (m_ARCameraManager != null)
+        {
+            m_ARCameraManager.enabled = active;
+        }
+
+        if (m_ARCameraBackground != null)
+        {
+            m_ARCameraBackground.enabled = active;
+        }
+    }
+
+    private IEnumerator FadePassthrough(bool active)
+    {
+        yield return new WaitForSeconds(2f);
     }
 }
