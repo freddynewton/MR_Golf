ab41a21e6d3eb6957893dec323872dcc5976ac01
Freddy Newton <freddakdogan2@gmail.com>
2025-01-07 19:45:30 +0100

    Update Golfball Behaviour and Golf Club Behaviour and updated AR Plane Visuals

---
+++ Assets/01. Scripts/GolfballController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class GolfballController : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float m_stoppingSpeed = 0.2f;
+    [SerializeField] private float m_forceMultiplier = 2.0f;
+
+    [Header("References")]
+    [SerializeField] private TrailRenderer m_trailRenderer;
+    [SerializeField] private Rigidbody m_rigidbody;
+
+    private void Awake()
+    {
+        // Get References
+        m_trailRenderer ??= GetComponent<TrailRenderer>();
+        m_rigidbody ??= GetComponent<Rigidbody>();
+
+        // Set Default Values
+        m_trailRenderer.emitting = false;
+    }
+
+    private void Update()
+    {
+        if (m_rigidbody.linearVelocity.magnitude < m_stoppingSpeed)
+        {
+            m_trailRenderer.emitting = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Golfclub"))
+        {
+            m_trailRenderer.emitting = true;
+
+            m_rigidbody.AddForce(collision.relativeVelocity * m_forceMultiplier, ForceMode.Impulse);
+        }
+    }
+}
